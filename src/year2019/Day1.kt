d122301e8e1ab8ece8ff9fb79b976f60b7419caa
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2019-12-03 23:09:17 +0100

    init changes

---
+++ src/year2019/Day1.kt
@@ -0,0 +1,14 @@
+package year2019
+
+fun main() = Day1.solve()
+
+object Day1 : AdventOfCode<String>("day1input.txt", {it.flatten()}){
+    override fun part1(): Any {
+        fileInput
+    }
+
+    override fun part2(): Any {
+        return ""
+    }
+
+}
